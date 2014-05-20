e252c1f2fd5b15d6e299525698323d54821b9b72
g3nj0 <jzurawel@gmail.com>
2014-05-20 07:12:42 -0400

    colorpicker
    added colorpicker
    - updates the rgb values for xhair sliders and text boxes

--- printcfg.js
+++ printcfg.js
@@ -3,17 +3,30 @@
 //    Created: 5/16/2014
 //=================================================================
 var printEvent;
-var updateDelay = 1000; // delay for which to update cfg text in milliseconds
+var updateDelay = 500; // delay for which to update cfg text in milliseconds
 
 test = function () {
     alert("meow");
 }
 
+writeCfgFile = function () {
+    var cfgTextArea = $("#cfgtextarea");
+    cfgTextArea.val(""); // clear text area
+
+    //var crosshairInputs = document.getElementsByName("crosshair");
+    $('label[for*=crosshair]').each(function (index) {
+        cfgTextArea.val(cfgTextArea.val() + $(this).text() + " " + $(this).next().val() + ";\r");
+    });
+    //cfgTextArea.val(cfgTextArea.val() + $('input[type=color]').val());
+}
+
+$(document).ready(writeCfgFile);
+
 // All inputs of type number will wait updateDelay milliseconds after changes are halted to update cfg text
 $(document).ready(function(){
     $('input[type=number]').change(function () {
         window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(test, updateDelay);
+        printEvent = window.setTimeout(writeCfgFile, updateDelay);
     })
 });
 
@@ -21,6 +34,33 @@ $(document).ready(function(){
 $(document).ready(function () {
     $('input[type=number]').keypress(function () {
         window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(test, updateDelay);
+        printEvent = window.setTimeout(writeCfgFile, updateDelay);
+    })
+});
+
+$(document).ready(function () {
+    $('input[type=range]').change(function () {
+        window.clearTimeout(printEvent);
+        printEvent = window.setTimeout(writeCfgFile, updateDelay);
+    })
+});
+
+//Transfers the color picked for the crosshair to the in
+$(document).ready(function () {
+    var crosshairColorpicker = $('#xhair_colorpicker');
+    crosshairColorpicker.change(function () {
+        var crosshairHexColor = crosshairColorpicker.val(); //#aabbcc
+        
+        var crosshairRed   = parseInt(crosshairHexColor.charAt(1) + crosshairHexColor.charAt(2), 16);
+        var crosshairGreen = parseInt(crosshairHexColor.charAt(3) + crosshairHexColor.charAt(4), 16);
+        var crosshairBlue  = parseInt(crosshairHexColor.charAt(5) + crosshairHexColor.charAt(6), 16);
+
+        $('#crosshaircolorr').val(crosshairRed);
+        $('#crosshaircolorr_slider').val(crosshairRed);
+        $('#crosshaircolorg').val(crosshairGreen);
+        $('#crosshaircolorg_slider').val(crosshairGreen);
+        $('#crosshaircolorb').val(crosshairBlue);
+        $('#crosshaircolorb_slider').val(crosshairBlue);
+        window.setTimeout(writeCfgFile, updateDelay);
     })
 });
