f7c3951066773903dc50418d7bac87df804d1d85
g3nj0 <jzurawel@gmail.com>
2014-06-01 17:52:41 -0400

    Added all input values to hash
    -- Awww yeah ^^ main functionality completed w the addition of all input
    values

--- input-controls.js
+++ input-controls.js
@@ -7,6 +7,7 @@
 //    of autoexec.cfg settings.
 //==============================================================
 
+//                 0                            10                            20                              30
 var inputBits  = [17, 8, 8, 6, 8, 9, 9, 9, 7, 7, 7, 7, 8, 8, 7, 7, 7, 7, 8, 7, 7, 12, 12, 5, 8, 8, 8, 8, 7, 9, 9, 9, 10, 10, 10, 7, 7, 7, 7, 8, 8, 8, 8, 7, 7, 8, 2];
 var inputBitTotal = 377;
 var selectBits = [3, 4, 2, 3, 2, 3, 3, 3, 2, 3];
@@ -19,7 +20,7 @@ for (var i = 0; i < inputBits.length; i++)
     meow += inputBits[i];
 
 alert(meow);
-*/
+//*/
 
 DecodeCfgValues = function () {
     var hashSet = window.location.hash.substring(1);
@@ -30,10 +31,11 @@ DecodeCfgValues = function () {
 
     // pick out the first hash
     var checkboxValues = Base62.decode(hashValues[0]).toString(2);
+    //var checkboxValues = Base62.decode(hashValues.splice(0,1)).toString(2);
     while (checkboxValues.length < $('input[type=checkbox]').length) { checkboxValues = "0" + checkboxValues; }
-    console.log(checkboxValues);
+    //console.log(checkboxValues);
 
-    $('input[type=checkbox').each(function (index) {
+    $('input[type=checkbox]').each(function (index) {
         if (checkboxValues.charAt(index) == "1")
             $(this).prop('checked', true);
         else
@@ -41,21 +43,16 @@ DecodeCfgValues = function () {
     })
 
     var selectBitString = Base62.decode(hashValues[1]).toString(2);
+    //var selectBitString = Base62.decode(hashValues.splice(0,1)).toString(2);
     while (selectBitString.length < selectBitTotal) { selectBitString = "0" + selectBitString; } // pad wit dem zeros
     //console.log(selectBitString);
     //console.log("selectBitsString length: " + selectBitString.length);
     var selectIndex = 0;
-    var selectIndexEnd = 0;
-    $('select').each(function (index) {
-        //console.log(selectIndex);
-        
+
+    $('select').each(function (index) {        
         var selectInfo = selectBitString.substring(selectIndex, (selectIndex += selectBits[index]));
         var selectValue = parseInt(selectInfo, 2);
-        /*selectIndex += selectBits[index];
-        console.log((selectIndex - 1));
-        console.log((parseInt(selectInfo, 2)));
-        console.log(selectInfo + "\n");
-        */
+
         if ($(this).is($('select[name=downloadfilter]'))) {
             if (selectValue == 0)
                 $(this).val("all");
@@ -69,6 +66,65 @@ DecodeCfgValues = function () {
         else
             $(this).val(selectValue);
     })
+    var blah = "";
+
+
+    /*
+    console.log(hashValues[2]);
+    console.log(hashValues[3]);
+    blah = Base62.decode(hashValues[2]).toString(2);
+    blah += Base62.decode(hashValues[3]).toString(2);
+    console.log(parseInt(blah.substring(16, 25), 2));
+    //*/
+    ///*
+
+    //console.log(inputBits.length);
+
+    // Gather all bits for the input data - 377 in total
+    var rangeBitString = (Base62.decode(hashValues[2])).toString(2);
+    var rangeStr = "";
+    //console.log(hashValues.length);
+    for (var i = 3; i < hashValues.length; i++)
+    {
+        rangeStr = (Base62.decode(hashValues[i])).toString(2);
+        
+        while (rangeStr.length < 32) { rangeStr = "0" + rangeStr; }
+        //console.log(rangeStr.length);
+        rangeBitString += rangeStr;
+    }
+
+    //console.log("rangebitString Length before padding: " + rangeBitString.length);
+    // pad with zeroes up to total length of all the input range values
+    while (rangeBitString.length < inputBitTotal) { rangeBitString = "0" + rangeBitString; }
+    //console.log("rangebitString Length: " + rangeBitString.length);
+    //console.log(rangeBitString);
+
+    var rangeNum = $('input[type=number]');
+    var rangeIndex = 0;
+    
+    $('input[type=range]').each(function (index) {
+        //console.log(rangeIndex);
+        var rangeInfo = rangeBitString.substring(rangeIndex, (rangeIndex += inputBits[index]));
+        var rangeValue = parseInt(rangeInfo, 2);
+        //console.log(rangeIndex);
+        //console.log(rangeValue + "\n");
+        if ($(this).attr('step') < 1){
+            rangeValue *= 0.01;
+
+            if (($(this).is($('#viewmodel_offset_x_slider'))) ||
+               ($(this).is($('#viewmodel_offset_y_slider'))) ||
+               ($(this).is($('#viewmodel_offset_z_slider')))) {
+                rangeValue -= 2;
+            }
+        }
+
+        if($(this).is($('#crosshairgap')))
+             rangeValue -= 100;
+
+        $(this).val(rangeValue);
+        rangeNum[index].value = rangeValue;
+    })
+    //*/
 };
 
 EncodeCfgValues = function () {
@@ -105,15 +161,16 @@ EncodeCfgValues = function () {
     })
     hashStr += "+" + Base62.encode(parseInt(binStr, 2));
 
+    ///*
     binStr = "";
     // grab all range type inputs and make a huge bit string
     $('input[type=range]').each(function (index) {
-        var s;
-        
+        var s = (+$(this).val());
+        var valueStr = "";
         // If the step is less than 1 than this is a float
         // Store it as an integer by multiplying the value by 100
         if ($(this).attr('step') < 1) {
-            s = (+$(this).val());
+            //s = (+$(this).val());
 
             if (($(this).is($('#viewmodel_offset_x_slider'))) ||
                 ($(this).is($('#viewmodel_offset_y_slider'))) ||
@@ -121,26 +178,29 @@ EncodeCfgValues = function () {
                 s += 2;
             }
 
-            s = (s * 100).toString(2);
-            while (s.length < inputBits[index]) { s = "0" + s; } // pad with zeroes
-            binStr += s;
+            s *= 100;
         }
-        else {
-            s = (+$(this).val());
-            if ($(this).is($('#crosshairgap_slider'))) {
-                s += 100;
-            }
+        else if ($(this).is($('#crosshairgap_slider'))){
+            s += 100;
+        }
+        //console.log(s.toString(2));
 
-            s = s.toString(2);
-            while (s.length < inputBits[index]) { s = "0" + s; } // pad with zeroes
-            binStr += s;
-        }
+        valueStr = s.toString(2);
+
+        while (valueStr.length < inputBits[index]) { valueStr = "0" + valueStr; } // pad with zeroes
+
+        if (valueStr.length != inputBits[index])
+            console.log(index + " : " + $(this).attr('id'));
+
+        binStr += valueStr;
     })
+    console.log(binStr.length);
 
     while ((binStr.length % 32) != 0) { binStr = "0" + binStr; } // pad with zeroes
 
     for (var i = 0; i < binStr.length; i += 32)
-        hashStr += "+" + Base62.encode(parseInt(binStr.substring(i, i + 31), 2));
+        hashStr += "+" + Base62.encode(parseInt(binStr.substring(i, i + 32), 2));
+    //*/
 
     window.location.hash = hashStr;
 };
