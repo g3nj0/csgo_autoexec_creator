ccb5064761a2e0a505c4a13ed7e3e8ed3bb36fbd
g3nj0 <jzurawel@gmail.com>
2014-06-01 09:04:10 -0400

    commands, base62 encoding, partial hash input save support,
    - Added all the commands I believe will be used
    - Found a base62 encoding script to include
    - Separated the hash encoding and textarea writing
    scripts(input-controls.js and printcfg.js)
    - All Checkboxes and Select boxes are saved in the location.hash (needs
    to not make a page jump in the future)

---
+++ input-controls.js
@@ -0,0 +1,260 @@
+﻿//==============================================================
+//    Author: Jesse "G3NJ0" Zurawel
+//    Created: 5/16/2014/
+//
+//    This file houses the script which will encode the input 
+//    values into the URL and also decode them to allow sharing
+//    of autoexec.cfg settings.
+//==============================================================
+
+var inputBits  = [17, 8, 8, 6, 8, 9, 9, 9, 7, 7, 7, 7, 8, 8, 7, 7, 7, 7, 8, 7, 7, 12, 12, 5, 8, 8, 8, 8, 7, 9, 9, 9, 10, 10, 10, 7, 7, 7, 7, 8, 8, 8, 8, 7, 7, 8, 2];
+var inputBitTotal = 377;
+var selectBits = [3, 4, 2, 3, 2, 3, 3, 3, 2, 3];
+var selectBitTotal = 28;
+
+/*
+var meow = 0;
+
+for (var i = 0; i < inputBits.length; i++)
+    meow += inputBits[i];
+
+alert(meow);
+*/
+
+DecodeCfgValues = function () {
+    var hashSet = window.location.hash.substring(1);
+
+    if (hashSet.length == 0) return;
+
+    var hashValues = hashSet.split("+");
+
+    // pick out the first hash
+    var checkboxValues = Base62.decode(hashValues[0]).toString(2);
+    while (checkboxValues.length < $('input[type=checkbox]').length) { checkboxValues = "0" + checkboxValues; }
+    console.log(checkboxValues);
+
+    $('input[type=checkbox').each(function (index) {
+        if (checkboxValues.charAt(index) == "1")
+            $(this).prop('checked', true);
+        else
+            $(this).prop('checked', false);
+    })
+
+    var selectBitString = Base62.decode(hashValues[1]).toString(2);
+    while (selectBitString.length < selectBitTotal) { selectBitString = "0" + selectBitString; } // pad wit dem zeros
+    //console.log(selectBitString);
+    //console.log("selectBitsString length: " + selectBitString.length);
+    var selectIndex = 0;
+    var selectIndexEnd = 0;
+    $('select').each(function (index) {
+        //console.log(selectIndex);
+        
+        var selectInfo = selectBitString.substring(selectIndex, (selectIndex += selectBits[index]));
+        var selectValue = parseInt(selectInfo, 2);
+        /*selectIndex += selectBits[index];
+        console.log((selectIndex - 1));
+        console.log((parseInt(selectInfo, 2)));
+        console.log(selectInfo + "\n");
+        */
+        if ($(this).is($('select[name=downloadfilter]'))) {
+            if (selectValue == 0)
+                $(this).val("all");
+            else if (selectValue == 1)
+                $(this).val("nosounds");
+            else if (selectValue == 2)
+                $(this).val("none");
+            else
+                $(this).val("mapsonly");
+        }
+        else
+            $(this).val(selectValue);
+    })
+};
+
+EncodeCfgValues = function () {
+    //alert("meow");
+    var hashStr = "";
+    var binStr = "";
+
+    // assemble all checkboxes into a binary string
+    $('input[type=checkbox]').each(function (index) {
+        $(this).prop('checked') ? binStr += "1" : binStr += "0"
+    })
+    hashStr += Base62.encode(parseInt(binStr, 2));
+
+    binStr = "";
+
+    $('select').each(function (index) {
+        var s = "";
+        s = $(this).find('option:selected').val();
+        var n = 0;
+        if ($(this).is($('select[name=downloadfilter]'))) {
+            if (s == "all")
+                s = 0;
+            else if (s == "nosounds")
+                s = 1;
+            else if (s == "none")
+                s = 2;
+            else
+                s = 3;
+        }
+
+        s = (+s).toString(2);
+        while (s.length < selectBits[index]) { s = "0" + s; }
+        binStr += s;
+    })
+    hashStr += "+" + Base62.encode(parseInt(binStr, 2));
+
+    binStr = "";
+    // grab all range type inputs and make a huge bit string
+    $('input[type=range]').each(function (index) {
+        var s;
+        
+        // If the step is less than 1 than this is a float
+        // Store it as an integer by multiplying the value by 100
+        if ($(this).attr('step') < 1) {
+            s = (+$(this).val());
+
+            if (($(this).is($('#viewmodel_offset_x_slider'))) ||
+                ($(this).is($('#viewmodel_offset_y_slider'))) ||
+                ($(this).is($('#viewmodel_offset_z_slider')))) {
+                s += 2;
+            }
+
+            s = (s * 100).toString(2);
+            while (s.length < inputBits[index]) { s = "0" + s; } // pad with zeroes
+            binStr += s;
+        }
+        else {
+            s = (+$(this).val());
+            if ($(this).is($('#crosshairgap_slider'))) {
+                s += 100;
+            }
+
+            s = s.toString(2);
+            while (s.length < inputBits[index]) { s = "0" + s; } // pad with zeroes
+            binStr += s;
+        }
+    })
+
+    while ((binStr.length % 32) != 0) { binStr = "0" + binStr; } // pad with zeroes
+
+    for (var i = 0; i < binStr.length; i += 32)
+        hashStr += "+" + Base62.encode(parseInt(binStr.substring(i, i + 31), 2));
+
+    window.location.hash = hashStr;
+};
+
+RGBToColorValue = function (r, g, b) {
+    var rhex = parseInt(r, 10).toString(16);
+    var ghex = parseInt(g, 10).toString(16);
+    var bhex = parseInt(b, 10).toString(16);
+
+    if (rhex.length == 1)
+        rhex = "0" + rhex;
+    if (ghex.length == 1)
+        ghex = "0" + ghex;
+    if (bhex.length == 1)
+        bhex = "0" + bhex;
+
+    return "#" + rhex + ghex + bhex;
+};
+
+$(document).ready(function () {
+    DecodeCfgValues();
+    writeCfgFile();
+
+    $('input[type=number]').change(function () {
+        window.clearTimeout(printEvent);
+        printEvent = window.setTimeout(writeCfgFile, updateDelay);
+    })
+    // Same as the function above but with .keypress as the event which triggers the update
+    $('input[type=number]').keypress(function () {
+        window.clearTimeout(printEvent);
+        printEvent = window.setTimeout(writeCfgFile, updateDelay);
+    })
+    // Updates the cfg text when a checkbox is changed
+    $('input[type=checkbox]').change(function () {
+        window.clearTimeout(printEvent);
+        printEvent = window.setTimeout(writeCfgFile, 0);
+    })
+    // When any slider is changed the writeCfgFile method is called
+    $('input[type=range]').change(function () {
+        window.clearTimeout(printEvent);
+        printEvent = window.setTimeout(writeCfgFile, updateDelay);
+    })
+    // When can select box is changed the writeCfgFile method is called
+    $("select").change(function () {
+        window.clearTimeout(printEvent);
+        printEvent = window.setTimeout(writeCfgFile, 0);
+    })
+});
+
+//Transfers the color picked for the crosshair to the input textboxes and sliders for each color
+$(document).ready(function () {
+    crosshairColorpicker.change(function () {
+        var crosshairHexColor = crosshairColorpicker.val(); // #rrggbb
+
+        //var crosshairRed = (crosshairHexColor.charAt(1) + crosshairHexColor.charAt(2)).toString();
+        var crosshairRed = parseInt(crosshairHexColor.charAt(1) + crosshairHexColor.charAt(2), 16);
+        var crosshairGreen = parseInt(crosshairHexColor.charAt(3) + crosshairHexColor.charAt(4), 16);
+        var crosshairBlue = parseInt(crosshairHexColor.charAt(5) + crosshairHexColor.charAt(6), 16);
+
+        crosshaircolorrObj.val(crosshairRed);
+        $('#crosshaircolorr_slider').val(crosshairRed);
+        crosshaircolorgObj.val(crosshairGreen);
+        $('#crosshaircolorg_slider').val(crosshairGreen);
+        crosshaircolorbObj.val(crosshairBlue);
+        $('#crosshaircolorb_slider').val(crosshairBlue);
+        window.setTimeout(writeCfgFile, 0);
+    })
+});
+
+// Send new color value to the color picker
+$(document).ready(function () {
+
+    // Red ===================
+    $('#crosshaircolorr').change(function () {
+        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    $('#crosshaircolorr').keypress(function () {
+        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    // Green ===================
+    $('#crosshaircolorg').change(function () {
+        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    $('#crosshaircolorg').keypress(function () {
+        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    // Blue ===================
+    $('#crosshaircolorb').change(function () {
+        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    $('#crosshaircolorb').keypress(function () {
+        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+
+    // Sliders ================
+    $('#crosshaircolorr_slider').change(function () {
+        $('#xhair_colorpicker').val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    $('#crosshaircolorg_slider').change(function () {
+        $('#xhair_colorpicker').val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+    $('#crosshaircolorb_slider').change(function () {
+        $('#xhair_colorpicker').val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
+    });
+
+    // Download ===============
+    $('#downloadButton').click(function (e) {
+        alert("meow");
+        $.generateFile({
+            filename: 'autoexec.cfg',
+            content: $('#cfgtextarea').val(),
+            script: 'download.php'
+        });
+
+        e.preventDefault();
+    });
+});
--- printcfg.js
+++ printcfg.js
@@ -1,6 +1,9 @@
 ﻿//=================================================================
 //    Author: Jesse "G3NJ0" Zurawel
 //    Created: 5/16/2014
+//
+//    Builds a large string of all of the autoexec.cfg which is 
+//    printed to a text area.
 //=================================================================
 var printEvent;
 var updateDelay = 500; // delay for which to update cfg text in milliseconds
@@ -9,37 +12,155 @@ var autoexecText;  // We will concatenate all of the autoexec text into this var
 var crosshairColorpicker = $('#xhair_colorpicker');
 var crosshaircolorrObj = $('#crosshaircolorr');
 var crosshaircolorgObj = $('#crosshaircolorg');
-var crosshaircolorbObj = $('#crosshaircolorb');
+var crosshaircolorbObj = $('#crosshaircolorb'); 
+
 
 writeCfgFile = function () {
     autoexecText = $('#cfgtextarea').val(""); // clear text area and autoexecText
 
     autoexecText = "// ================== Rates ===================\n\n";
-    appendNewCommand("rate", $("#rate_slider"));
-    appendNewCommand("cl_cmdrate", $("#cmdrate_slider"));
-    appendNewCommand("cl_updaterate", $("#updaterate_slider"));
-    appendNewCommand("cl_interp", $("#interp_slider"));
-    appendNewCommand("cl_interp_ratio", $("#interp_ratio"));
+    appendNewCommand("rate", $("#rate_slider"));                        // 17
+    appendNewCommand("cl_cmdrate", $("#cmdrate_slider"));               // 8
+    appendNewCommand("cl_updaterate", $("#updaterate_slider"));         // 8
+    appendNewCommand("cl_interp", $("#interp_slider"));                 // 6
+    appendNewCommand("cl_interp_ratio", $("#interp_ratio_slider"));     // 8
+
+    autoexecText += "\n// ============== Video settings ==============\n\n";
+    appendNewCommand("mat_monitorgamma", $('#gamma_slider'));           // 9
+    appendNewCommand("fps_max", $('#maxfps_slider'));                   // 9
+    appendNewCommand("fps_max_menu", $('#maxfps_menu_slider'));         // 9
+    appendNewCheckedCommand("mat_vsync", $('#vsync'));                  
+    appendNewCheckedCommand("r_dynamic", $('#dynshadow'));
+    appendNewCheckedCommand("r_drawtracers_firstperson", $('#fptracer'));
+    autoexecText += "mat_savechanges\n";
+
+    autoexecText += "\n// ============== Audio settings ==============\n\n";
+    appendNewCommand("volume", $('#volume_slider'));                    // 7
+    appendNewCheckedCommand("voice_enable", $('#voiceenable'));
+    appendNewCommand("voice_scale", $('#voicescale_slider'));           // 7 -10
+
+    // Special case for speaker config
+    autoexecText += "windows_speaker_config \"";
+    if ($("select[name=speaker]").val() == 0)
+        autoexecText += "-1";
+    else
+        autoexecText += $("select[name=speaker]").val();                //== 3
+
+    autoexecText += "\"\n";
+
+    appendNewCommand("snd_musicvolume", $("#music_slider"));                   // 7
+    appendNewCommand("snd_mixahead", $("#soundmixahead_slider"));              // 7
+    appendNewCommand("snd_headphone_pan_exponent", $("#panexp_slider"));       // 8
+    appendNewCommand("snd_headphone_pan_radial_weight", $("#panrad_slider"));  // 8
+    appendNewCheckedCommand("snd_legacy_surround", $('#legacy'));
+    appendNewCheckedCommand("snd_mute_losefocus", $('#mutelosefocus'));
+    appendNewCheckedCommand("lobby_voice_chat_enabled", $('#voiceenablelobby'));
 
     autoexecText += "\n// =============== HUD settings ===============\n\n";
-    appendNewCommand("cl_showfps", $("#showfps"));
+    appendNewCommand("hud_scaling", $("#hudscale_slider"));             // 7
+    appendNewCommand("safezonex", $("#hudsafex_slider"));               // 7
+    appendNewCommand("safezoney", $("#hudsafey_slider"));               // 7
+    appendNewCommand("cl_hud_background_alpha", $("#hudalpha_slider")); // 7
+    appendNewCommand("cl_hud_color", $("#hudcolor"));                   // == 4
+    appendNewCheckedCommand("cl_hud_playercount_showcount", $("#showplayercount"));
+    appendNewCheckedCommand("cl_hud_playercount_pos", $("#playercountpos"));
+    appendNewCheckedCommand("cl_show_clan_in_death_notice", $("#showclandeath"));
+    appendNewCheckedCommand("cl_hud_bomb_under_radar", $("#bombunderradar"));
+    appendNewCheckedCommand("cl_hud_healthammo_style", $("#healthammo"));
+    appendNewCheckedCommand("cl_showloadout", $("#showloadout"));
+    appendNewCheckedCommand("cl_loadout_colorweaponnames", $("#loadoutcolor"));
+    appendNewCheckedCommand("hud_showtargetid", $("#showtargetid"));
+    //================================================================
+    autoexecText += "\n";
+    appendNewCommand("cl_hud_radar_scale", $("#hudradarscale_slider"));       // 8
+    appendNewCommand("cl_radar_scale", $("#radarmapscale_slider"));           // 7
+    appendNewCommand("cl_radar_icon_scale_min", $("#radariconscale_slider")); // 7 -21
+    appendNewCheckedCommand("cl_radar_always_centered", $("#radarcentered"));
+    appendNewCheckedCommand("cl_radar_rotate", $("#radarrotates"));
+    //================================================================
+    autoexecText += "\n";
+    appendNewCheckedCommand("developer", $("#developer"));
+
+    //appendNewCommand("con_filter_enable", $("select[name=confilterenable]"));  //== 2
+    if ($("select[name=confilterenable]").val() == 0)
+        autoexecText += "con_filter_enable \"0\"\ncon_filter_text \"\"\n";
+    else if ($("select[name=confilterenable]").val() == 1)
+        autoexecText += "con_filter_enable \"1\"\ncon_filter_text \"damage\"\n";
+    else
+        autoexecText += "con_filter_enable \"1\"\ncon_filter_text \"damage\"\n";
+
+    if ($("#filterplayer").prop("checked"))
+        autoexecText += "con_filter_text_out \"Player:\"\n";
+
+    appendNewCheckedCommand("gameinstructor_enable", $("#gameinstruct"));
+    //================================================================
+    autoexecText += "\n";
+    appendNewCommand("cl_showfps", $("#showfps"));                                        //== 3
     appendNewCheckedCommand("net_graph", $("#netgraph"));
     appendNewCheckedCommand("net_graphproportionalfont", $("#netgraphfont"));
-    appendNewCommand("net_graphheight", $("#netgraphheight_slider"));
-    appendNewCommand("net_graphpos", $("#netgraphpos_slider"));
+    appendNewCommand("net_graphheight", $("#netgraphheight_slider"));                     // 12
+    appendNewCommand("net_graphpos", $("#netgraphpos_slider"));                           // 12
 
-    autoexecText += "\n// ============ Crosshair settings ============\n\n";
-    appendNewCommand("cl_crosshairstyle", $("select[name=crosshairstyle]"));
-    $('label[for*=crosshaircolor]').each(function (index) {
-        appendNewCommand($(this).text(), $(this).next());
-    })
-    appendNewCommand("cl_crosshairalpha", $("#crosshairalpha_slider"));
-    appendNewCheckedCommand("cl_crosshairdot", $("#crosshairdot"));
+    autoexecText += "\n// ============= Viewmodel settings =============\n\n";
+    appendNewCommand("cl_bob_lower_amt", $("#lowerbobamt_slider"));                       // 5
+    appendNewCommand("cl_bobamt_lat", $("#boblat_slider"));                               // 8
+    appendNewCommand("cl_bobamt_vert", $("#bobvert_slider"));                             // 8
+    appendNewCommand("cl_viewmodel_shift_left_amt", $("#viewmodel_shift_left_slider"));   // 8
+    appendNewCommand("cl_viewmodel_shift_right_amt", $("#viewmodel_shift_right_slider")); // 8
+    // ========================================================================
+    appendNewCommand("viewmodel_presetpos", $("select[name=viewmodel_preset]")); //== 2
+    appendNewCommand("viewmodel_fov", $("#viewmodel_fov"));                      // 7
+    appendNewCommand("viewmodel_offset_x", $("#viewmodel_offset_x_slider"));     // 9
+    appendNewCommand("viewmodel_offset_y", $("#viewmodel_offset_y_slider"));     // 9
+    appendNewCommand("viewmodel_offset_z", $("#viewmodel_offset_z_slider"));     // 9
+    
+    autoexecText += "\n// =============== Mouse settings ===============\n\n"; // Mouse
+    appendNewCommand("sensitivity", $('#sens_slider'));                     // 10
+    appendNewCommand("zoom_sensitivity_ratio_mouse", $('#zoom_slider'));    // 10
+    appendNewCheckedCommand("m_rawinput", $("#raw"));                       
+    appendNewCommand("m_customaccel", $('#accel_slider'));                  // 10
 
-    autoexecText = autoexecText + "\nhost_writeconfig\n";
+    autoexecText += "\n// ============ Crosshair settings ============\n\n";  // Crosshair
+    appendNewCommand("cl_crosshairstyle", $("select[name=crosshairstyle]"));                        //== 3
+    appendNewCommand("cl_crosshair_dynamic_maxdist_splitratio",$('#crosshairdynmaxdist_slider'));   // 7
+    appendNewCommand("cl_crosshair_dynamic_splitalpha_innermod",$('#crosshairalphainner_slider'));  // 7
+    appendNewCommand("cl_crosshair_dynamic_splitalpha_outermod", $('#crosshairalphaouter_slider')); // 7
+    appendNewCommand("cl_crosshair_dynamic_splitdist", $('#crosshairsplitdist_slider'));            // 7
+    appendNewCommand("cl_crosshaircolor", $("select[name=crosshaircolor]"));                        //== 3
+    // ========================================================================
+    appendNewCommand("cl_crosshaircolor_r", $("#crosshaircolorr_slider"));  // 8
+    appendNewCommand("cl_crosshaircolor_g", $("#crosshaircolorg_slider"));  // 8
+    appendNewCommand("cl_crosshaircolor_b", $("#crosshaircolorb_slider"));  // 8
+    appendNewCommand("cl_crosshairalpha", $("#crosshairalpha_slider"));     // 8
+    // ========================================================================
+    appendNewCommand("cl_crosshairsize", $("#crosshairsize_slider"));           // 7
+    appendNewCommand("cl_crosshairthickness", $("#crosshairthickness_slider")); // 7
+    appendNewCommand("cl_crosshairgap", $("#crosshairgap_slider"));             // 8
+    appendNewCheckedCommand("cl_crosshairdot", $("#crosshairdot"));              
+    appendNewCheckedCommand("cl_crosshair_drawoutline", $("#crosshairoutline"));
+    appendNewCommand("cl_crosshair_outlinethickness", $("#crosshairoutlinethickness_slider")); // 2
+
+    autoexecText += "\n// ============== MISC settings ==============\n\n";  // MISC
+    appendNewCheckedCommand("cl_autohelp", $("#autohelp"));
+    appendNewCheckedCommand("cl_autowepswitch", $("#autoweapon"));
+    appendNewCheckedCommand("cl_righthand", $("#righthand"));
+    appendNewCheckedCommand("cl_disablefreezecam", $("#freezecam"));
+    appendNewCheckedCommand("cl_freezecameffects_showholiday", $("#holidayeffect"));
+    appendNewCheckedCommand("cl_disablehtmlmotd", $("#disablemotd"));
+    appendNewCheckedCommand("cl_forcepreload", $("#forcepreload"));
+    appendNewCommand("cl_downloadfilter", $("select[name=downloadfilter]"));        //== 2
+    appendNewCommand("cl_teammate_colors_show", $("select[name=teammatecolors]"));  //== 2
+    appendNewCommand("cl_color", $("select[name=selfcolor]"));                      //== 3
+    appendNewCheckedCommand("cl_use_opens_buy_menu", $("#usebuymenu"));
+    appendNewCheckedCommand("closeonbuy", $("#closeonbuy"));
+    appendNewCheckedCommand("option_duck_method", $("#toggleduck"));
+    appendNewCheckedCommand("option_speed_method", $("#togglewalk"));
+
+    autoexecText += "\nhost_writeconfig\n";
     $('#cfgtextarea').val(autoexecText);
+    EncodeCfgValues();
 };
-$(document).ready(writeCfgFile);
+//$(document).ready(writeCfgFile);
 
 // Appends a new command to the autoexecText variable
 // commandName:String  domObj:DOMObject (object which you want the corresponding value of)
@@ -52,116 +173,29 @@ appendNewCheckedCommand = function (commandName, checkObj) {
     autoexecText += commandName + " \"" + (checkObj.prop("checked") ? 1 : 0) + "\"\n";  
 };
 
-RGBToColorValue = function (r, g, b) {
-    var rhex = parseInt(r, 10).toString(16);
-    var ghex = parseInt(g, 10).toString(16);
-    var bhex = parseInt(b, 10).toString(16);
+StringToBinary = function (str) {
+    var b;
+    var data = "";
+    for (var i = 0; i < str.length; i++)
+    {
+        b = str.charCodeAt(i).toString(2);
+        while (b.length < 8) { b = "0" + b; } // pad with zeroes
+        
+        data += b;
+    }
 
-    if (rhex.length == 1)
-        rhex = "0" + rhex;
-    if (ghex.length == 1)
-        ghex = "0" + ghex;
-    if (bhex.length == 1)
-        bhex = "0" + bhex;
+    while ((data.length % 64) != 0) { data = "0" + data; }
 
-    return "#" + rhex + ghex + bhex;
+    return data;
 };
 
-$(document).ready(function () {
-    // All inputs of type number will wait updateDelay milliseconds after changes are halted to update cfg text
-    $('input[type=number]').change(function () {
-        window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(writeCfgFile, updateDelay);
-    })
-    // Same as the function above but with .keypress as the event which triggers the update
-    $('input[type=number]').keypress(function () {
-        window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(writeCfgFile, updateDelay);
-    })
-    // Updates the cfg text when a checkbox is changed
-    $('input[type=checkbox]').change(function () {
-        window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(writeCfgFile, 0);
-    })
-    // When any slider is changed the writeCfgFile method is called
-    $('input[type=range]').change(function () {
-        window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(writeCfgFile, updateDelay);
-    })
-    // When can select box is changed the writeCfgFile method is called
-    $("select").change(function ()
-    {
-        window.clearTimeout(printEvent);
-        printEvent = window.setTimeout(writeCfgFile, 0);
-    })
-});
+EncodeBinaryToBase62 = function (bin) {
+    var sixtytwostr = "";
+    for(var i = 0; i < bin.length; i+=64)
+        sixtytwostr += Base62.encode(parseInt(bin.substring(i, i+63), 2)) + "/";
 
-//Transfers the color picked for the crosshair to the input textboxes and sliders for each color
-$(document).ready(function () {
-    crosshairColorpicker.change(function () {
-        var crosshairHexColor = crosshairColorpicker.val(); // #rrggbb
-        
-        //var crosshairRed = (crosshairHexColor.charAt(1) + crosshairHexColor.charAt(2)).toString();
-        var crosshairRed = parseInt(crosshairHexColor.charAt(1) + crosshairHexColor.charAt(2), 16);
-        var crosshairGreen = parseInt(crosshairHexColor.charAt(3) + crosshairHexColor.charAt(4), 16);
-        var crosshairBlue  = parseInt(crosshairHexColor.charAt(5) + crosshairHexColor.charAt(6), 16);
-
-        crosshaircolorrObj.val(crosshairRed);
-        $('#crosshaircolorr_slider').val(crosshairRed);
-        crosshaircolorgObj.val(crosshairGreen);
-        $('#crosshaircolorg_slider').val(crosshairGreen);
-        crosshaircolorbObj.val(crosshairBlue);
-        $('#crosshaircolorb_slider').val(crosshairBlue);
-        window.setTimeout(writeCfgFile, 0);
-    })
-});
-
-// Send new color value to the color picker
-$(document).ready(function () {
-    // Red ===================
-    $('#crosshaircolorr').change(function () {
-        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    $('#crosshaircolorr').keypress(function () {
-        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    // Green ===================
-    $('#crosshaircolorg').change(function () {
-        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    $('#crosshaircolorg').keypress(function () {
-        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    // Blue ===================
-    $('#crosshaircolorb').change(function () {
-        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    $('#crosshaircolorb').keypress(function () {
-        crosshairColorpicker.val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-
-    //Sliders
-    $('#crosshaircolorr_slider').change(function () {
-        $('#xhair_colorpicker').val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    $('#crosshaircolorg_slider').change(function () {
-        $('#xhair_colorpicker').val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-    $('#crosshaircolorb_slider').change(function () {
-        $('#xhair_colorpicker').val(RGBToColorValue(crosshaircolorrObj.val(), crosshaircolorgObj.val(), crosshaircolorbObj.val()));
-    });
-});
+    return sixtytwostr;
+};
 
 
-$(document).ready(function () {
-    $('#downloadButton').click(function (e) {
-        alert("meow");
-        $.generateFile({
-            filename: 'autoexec.cfg',
-            content: $('#cfgtextarea').val(),
-            script: 'download.php'
-        });
 
-        e.preventDefault();
-    });
-});
